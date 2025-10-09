1d6a8c0276d9d34111f4d194feda38484e96ea17
Drasenix <adowns@hotmail.fr>
2025-10-09 16:32:02 +0200

    useGroup custom hook

--- frontend/demo-categories/src/components/SelectCategoryComponent.tsx
+++ frontend/demo-categories/src/components/SelectCategoryComponent.tsx
@@ -1,18 +1,20 @@
+import { useState } from "react";
+import useGroups from "../hooks/categories/useGroups";
 import { IGroup } from "../services/api/interfaces/Categorie";
 
 interface ISelectCategoryComponentProps {
-  groups: IGroup[];
   selectFilterGroupId: Function;
 }
 
 function SelectCategoryComponent(props: ISelectCategoryComponentProps) {
+  const [groupsAvailable] = useState<IGroup[]>(useGroups());
   return (
     <select
       className="list-categories-select"
       onChange={(event) => props.selectFilterGroupId(event.target.value)}
     >
       <option value="all">Tous les groupes de catégories</option>
-      {props.groups.map((group) => (
+      {groupsAvailable.map((group) => (
         <option
           key={group.id}
           value={group.id}
